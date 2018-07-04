3d5f25cf176e214560a07b107f9cf7538f7cbb22
Nils Hesse <n.hesse@patagona.de>
2018-07-04 15:22:33 +0200

    PM-3465 Add scalastyle to test task dependencies

--- src/main/scala/com.patagona.codestylesettings/CodeStyleSettingsPlugin.scala
+++ src/main/scala/com.patagona.codestylesettings/CodeStyleSettingsPlugin.scala
@@ -1,16 +1,29 @@
+package com.patagona.codestylesettings
+
 import com.lucidchart.sbt.scalafmt.ScalafmtCorePlugin.autoImport._
 import org.scalastyle.sbt.ScalastylePlugin.autoImport._
-import sbt.Keys.baseDirectory
+import sbt.Keys._
 import sbt._
 
 object CodeStyleSettingsPlugin extends AutoPlugin {
+  object autoImport {
+    val testScalaStyle = taskKey[Unit]("testScalaStyle")
+    val compileScalastyle = taskKey[Unit]("compileScalastyle")
+  }
+
+  import autoImport._
+
   override def requires: Plugins = org.scalastyle.sbt.ScalastylePlugin && com.lucidchart.sbt.scalafmt.ScalafmtCorePlugin
   override def trigger: PluginTrigger = allRequirements
   override def projectSettings: Seq[sbt.Def.Setting[_]] = {
     Seq(
       scalastyleConfig := (baseDirectory in ThisBuild).value / "project" / "scalastyle_config.xml",
       scalastyleConfig in Test := (baseDirectory in ThisBuild).value / "project" / "scalastyle_config.xml",
-      scalafmtVersion := "1.5.1"
+      scalafmtVersion := "1.5.1",
+      testScalaStyle := scalastyle.in(Test).toTask("").value,
+      compileScalastyle := scalastyle.in(Compile).toTask("").value,
+      (test in Test) := ((test in Test) dependsOn testScalaStyle).value,
+      (test in Test) := ((test in Test) dependsOn compileScalastyle).value
     )
   }
 }
